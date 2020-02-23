635a982788962239d2dba299be5e987a541f7cc9
goosebumps98 <61392202+goosebumps98@users.noreply.github.com>
2020-02-23 15:47:53 -0500

    Update CmdJoin.java

--- src/com/massivecraft/factions/cmd/CmdJoin.java
+++ src/com/massivecraft/factions/cmd/CmdJoin.java
@@ -10,7 +10,7 @@ public class CmdJoin extends FCommand {
         super();
         this.aliases.add("join");
 
-        this.requiredArgs.add("faction name");
+        this.requiredArgs.add("league name");
         this.optionalArgs.put("player", "you");
 
         this.permission = Permission.JOIN.node;
@@ -31,12 +31,12 @@ public class CmdJoin extends FCommand {
         boolean samePlayer = fplayer == fme;
 
         if (!samePlayer && !Permission.JOIN_OTHERS.has(sender, false)) {
-            msg("<b>You do not have permission to move other players into a faction.");
+            msg("<b>You do not have permission to move other players into a league.");
             return;
         }
 
         if (!faction.isNormal()) {
-            msg("<b>Players may only join normal factions. This is a system faction.");
+            msg("<b>Players may only join normal league. This is a system faction.");
             return;
         }
 
@@ -46,29 +46,29 @@ public class CmdJoin extends FCommand {
         }
 
         if (Conf.factionMemberLimit > 0 && faction.getFPlayers().size() >= Conf.factionMemberLimit) {
-            msg(" <b>!<white> The faction %s is at the limit of %d members, so %s cannot currently join.", faction.getTag(fme), Conf.factionMemberLimit, fplayer.describeTo(fme, false));
+            msg(" <b>!<white> The league %s is at the limit of %d members, so %s cannot currently join.", faction.getTag(fme), Conf.factionMemberLimit, fplayer.describeTo(fme, false));
             return;
         }
 
         if (fplayer.hasFaction()) {
-            msg("<b>%s must leave %s current faction first.", fplayer.describeTo(fme, true), (samePlayer ? "your" : "their"));
+            msg("<b>%s must leave %s current league first.", fplayer.describeTo(fme, true), (samePlayer ? "your" : "their"));
             return;
         }
 
         if (!Conf.canLeaveWithNegativePower && fplayer.getPower() < 0) {
-            msg("<b>%s cannot join a faction with a negative power level.", fplayer.describeTo(fme, true));
+            msg("<b>%s cannot join a league with a negative power level.", fplayer.describeTo(fme, true));
             return;
         }
 
         if (!(faction.getOpen() || faction.isInvited(fplayer) || fme.isAdminBypassing() || Permission.JOIN_ANY.has(sender, false))) {
-            msg("<i>This faction requires invitation.");
+            msg("<i>This league requires invitation.");
             if (samePlayer)
-                faction.msg("%s<i> tried to join your faction.", fplayer.describeTo(faction, true));
+                faction.msg("%s<i> tried to join your league.", fplayer.describeTo(faction, true));
             return;
         }
 
         // if economy is enabled, they're not on the bypass list, and this command has a cost set, make sure they can pay
-        if (samePlayer && !canAffordCommand(Conf.econCostJoin, "to join a faction")) return;
+        if (samePlayer && !canAffordCommand(Conf.econCostJoin, "to join a league")) return;
 
         // trigger the join event (cancellable)
         FPlayerJoinEvent joinEvent = new FPlayerJoinEvent(FPlayers.i.get(me), faction, FPlayerJoinEvent.PlayerJoinReason.COMMAND);
@@ -76,13 +76,13 @@ public class CmdJoin extends FCommand {
         if (joinEvent.isCancelled()) return;
 
         // then make 'em pay (if applicable)
-        if (samePlayer && !payForCommand(Conf.econCostJoin, "to join a faction", "for joining a faction")) return;
+        if (samePlayer && !payForCommand(Conf.econCostJoin, "to join a league", "for joining a league")) return;
 
         fme.msg("<i>%s successfully joined %s.", fplayer.describeTo(fme, true), faction.getTag(fme));
 
         if (!samePlayer)
-            fplayer.msg("<i>%s moved you into the faction %s.", fme.describeTo(fplayer, true), faction.getTag(fplayer));
-        faction.msg("<i>%s joined your faction.", fplayer.describeTo(faction, true));
+            fplayer.msg("<i>%s moved you into the league.", fme.describeTo(fplayer, true), faction.getTag(fplayer));
+        faction.msg("<i>%s joined your league.", fplayer.describeTo(faction, true));
 
         fplayer.resetFactionData();
         fplayer.setFaction(faction);
@@ -90,9 +90,9 @@ public class CmdJoin extends FCommand {
 
         if (Conf.logFactionJoin) {
             if (samePlayer)
-                P.p.log("%s joined the faction %s.", fplayer.getName(), faction.getTag());
+                P.p.log("%s joined the league %s.", fplayer.getName(), faction.getTag());
             else
-                P.p.log("%s moved the player %s into the faction %s.", fme.getName(), fplayer.getName(), faction.getTag());
+                P.p.log("%s moved the player %s into the league %s.", fme.getName(), fplayer.getName(), faction.getTag());
         }
     }
 }
